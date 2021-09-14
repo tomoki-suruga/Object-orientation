7c8255c6e0e05ea84049f627bfba85b7d2c5ab2f
tomoki-suruga <tomoki.s.0927@gmail.com>
2021-09-14 18:46:18 +0900

    finish PlayerClass

--- src/babanuki/Player.java
+++ src/babanuki/Player.java
@@ -13,5 +13,42 @@ public class Player {
 		this.table = table;
 	}
 	
+	public void Play(Player nextPlayer) {
+		Hand nextHnad = nextPlayer.showHand();
+		Card pickedCard = nextHnad.pickCard();
+		System.out.println(this + ":" + nextPlayer + "さんから" + pickedCard + "を引きました");
+		dealCard(pickedCard);
+		
+		if(myHand.getNumberOfCards() == 0) {
+			master.declareWin(this);
+		} else {
+			System.out.println(this + ":残りの手札は" + myHand + "です");
+		}
+	}
 	
+	public Hand showHand() {
+		if(myHand.getNumberOfCards() == 1) {
+			master.declareWin(this);
+		}
+		myHand.shuffle();
+		return myHand;
+	}
+	
+	public void receiveCard(Card card) {
+		dealCard(card);
+	}
+	
+	private void dealCard(Card card) {
+		myHand.addCard(card);
+		Card[] sameCards = myHand.findSameNumberCard();
+		
+		if(sameCards != null) {
+			System.out.println(this + ":");
+			table.disposeCard(sameCards);
+		}
+	}
+	
+	public String toString() {
+		return name;
+	}
 }
