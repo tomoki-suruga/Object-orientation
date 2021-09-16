dd131575065ea8cceddae804d286e67cdd96262c
tomoki-suruga <tomoki.s.0927@gmail.com>
2021-09-16 18:16:21 +0900

    add prepareGame

--- src/babanuki/Master.java
+++ src/babanuki/Master.java
@@ -5,8 +5,18 @@ import java.util.ArrayList;
 public class Master {
 	public ArrayList players = new ArrayList();
 	
-	public void prepareGame(Hand Cards) {
+	public void prepareGame(Hand cards) {
+		System.out.println("【カードを配ります】");
+		cards.shuffle();
+		int numberOfCards = cards.getNumberOfCards();
+		int numberOfPlayers = players.size();
 		
+		for(int index = 0; index < numberOfCards; index++) {
+			Card card = cards.pickCard();
+			
+			Player player = (Player)players.get(index % numberOfPlayers);
+			player.receiveCard(card);
+		}
 	}
 
 }
